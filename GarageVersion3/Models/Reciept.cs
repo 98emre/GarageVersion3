869b0c18a22d18c78d733d425e8ac6ac2abb41c7
98emre <emre-98@live.se>
2024-05-02 10:56:39 +0200

    feat: added model for User and Receipt

---
+++ GarageVersion3/Models/Reciept.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GarageVersion3.Models
+{
+    public class Reciept
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        [Range(1,int.MaxValue,ErrorMessage = "No negative value")]
+        public int ParkingSpot { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        [Range(0.0, double.MaxValue, ErrorMessage = "Price can't be less than 0")]
+        public double Price { get; set; }
+
+
+    }
+}
---
+++ GarageVersion3/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GarageVersion3.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Range(10,12,ErrorMessage = "Birth Date must be between 10 - 12")]
+        public string BirthDate { get; set; }
+
+        [Range(2,32, ErrorMessage = "First Name must be between 2 - 32")]
+        public string FirstName { get; set; }
+
+        [Range(2, 32, ErrorMessage = "Last Name must be between 2 - 32")]
+        public string LastName { get; set; }
+        
+        List<Reciept> Reciepts { get; set; }
+    }
+}
