0ee563667ab95a595c9bfcf6f8a310e8497fb572
98emre <emre-98@live.se>
2024-05-02 10:33:51 +0200

    Add project files.

---
+++ GarageVersion3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GarageVersion3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
