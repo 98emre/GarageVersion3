b946a2da7916a5dfc6c3225d9d701d545a84656a
98emre <emre-98@live.se>
2024-05-02 14:14:26 +0200

    feat: added seed data and change registrationnumber datatype from int to string

--- GarageVersion3/Data/GarageVersion3Context.cs
+++ GarageVersion3/Data/GarageVersion3Context.cs
@@ -19,5 +19,18 @@ namespace GarageVersion3.Data
         public DbSet<GarageVersion3.Models.User> User { get; set; } = default!;
         public DbSet<GarageVersion3.Models.Vehicle> Vehicle { get; set; } = default!;
         public DbSet<GarageVersion3.Models.VehicleType> VehicleType { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<VehicleType>().HasData(
+                new VehicleType { Id = 1, Type = "Car", ParkingSize = 1 },
+                new VehicleType { Id = 2, Type = "Bus", ParkingSize = 1 },
+                new VehicleType { Id = 3, Type = "Truck", ParkingSize = 2 },
+                new VehicleType { Id = 4, Type = "Boat", ParkingSize = 3 },
+                new VehicleType { Id = 5, Type = "Airplane", ParkingSize = 3 }
+            );
+        }
     }
 }
