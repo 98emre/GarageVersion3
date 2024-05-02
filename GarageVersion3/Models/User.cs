b7f885b66522f613c6ea59a4a776fbe76e3ecdb2
98emre <emre-98@live.se>
2024-05-02 13:02:22 +0200

    fix: change string annoation Range to StringLenght

--- GarageVersion3/Models/User.cs
+++ GarageVersion3/Models/User.cs
@@ -7,14 +7,15 @@ namespace GarageVersion3.Models
         [Key]
         public int Id { get; set; }
 
-        [Range(10,12,ErrorMessage = "Birth Date must be between 10 - 12")]
+        [StringLength(12, MinimumLength = 10, ErrorMessage = "Birth Date must be between 10 - 12")]
         public string BirthDate { get; set; }
 
-        [Range(2,32, ErrorMessage = "First Name must be between 2 - 32")]
+        [StringLength(32, MinimumLength = 2, ErrorMessage = "First Name must be between 2 - 32")]
         public string FirstName { get; set; }
 
-        [Range(2, 32, ErrorMessage = "Last Name must be between 2 - 32")]
+        [StringLength(32, MinimumLength = 2, ErrorMessage = "Last Name must be between 2 - 32")]
         public string LastName { get; set; }
+
         public List<Vehicle> Vehicles { get; set; }
         public List<Receipt> Receipts { get; set; }
     }
