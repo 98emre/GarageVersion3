841d1c78df1eb70eb71a031a34b6f1f5ed4c12b7
98emre <emre-98@live.se>
2024-05-06 16:13:34 +0200

    feat: you can add vehile types

---
+++ GarageVersion3/Controllers/VehicleTypeController.cs
@@ -0,0 +1,44 @@
+﻿using GarageVersion3.Data;
+using GarageVersion3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GarageVersion3.Controllers
+{
+    public class VehicleTypeController : Controller
+    {
+        private readonly GarageVersion3Context _context;
+
+        public VehicleTypeController(GarageVersion3Context context)
+        {
+            _context = context;
+        }
+
+        // GET: VehicleTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VehicleTypes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(VehicleType vehicleType)
+        {
+            var existingType = await _context.VehicleType.FirstOrDefaultAsync(vt => vt.Type.Trim().ToUpper() == vehicleType.Type.ToUpper());
+
+            if (existingType != null)
+            {
+                ModelState.AddModelError(nameof(vehicleType.Type), "A vehicle type with this name already exists");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(vehicleType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Create","Vehicles");
+            }
+            return View(vehicleType);
+        }
+    }
+}
