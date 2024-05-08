e65dd11dc6c3dce65b2c03c0e80bab2fa4b7bb67
98emre <emre-98@live.se>
2024-05-08 23:33:23 +0200

    feat: added statistics page on home controller instead

--- GarageVersion3/Controllers/HomeController.cs
+++ GarageVersion3/Controllers/HomeController.cs
@@ -1,16 +1,18 @@
+using GarageVersion3.Data;
 using GarageVersion3.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace GarageVersion3.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly GarageVersion3Context _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(GarageVersion3Context context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -18,10 +20,38 @@ namespace GarageVersion3.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpGet]
+        public async Task<IActionResult> Statistics()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var parkedVehicles = await _context.Vehicle
+                .Where(v => _context.ParkingLot.Any(pt => pt.VehicleId == v.Id))
+                .Include(v => v.VehicleType)
+                .ToListAsync();
+
+            var vehicleTypeCount = new Dictionary<string, int>();
+
+            foreach (var vehicle in parkedVehicles)
+            {
+                if (vehicle.VehicleType != null)
+                {
+                    if (!vehicleTypeCount.ContainsKey(vehicle.VehicleType.Type))
+                    {
+                        vehicleTypeCount[vehicle.VehicleType.Type] = 0;
+                    }
+
+                    vehicleTypeCount[vehicle.VehicleType.Type]++;
+                };
+            }
+
+            var totalWheels = parkedVehicles.Sum(v => v.NrOfWheels);
+            var totalRevenue = _context.Receipt.Sum(r => r.Price);
+
+            ViewBag.vehicleTypeCount = vehicleTypeCount;
+            ViewBag.TotalWheels = totalWheels;
+            ViewBag.TotalRevenue = totalRevenue.ToString("#,##0.00");
+
+
+            return View();
         }
     }
 }
--- GarageVersion3/Controllers/VehiclesController.cs
+++ GarageVersion3/Controllers/VehiclesController.cs
@@ -412,43 +412,9 @@ namespace GarageVersion3.Controllers
             {
                 Text = vt.Type,
                 Value = vt.Id.ToString()
-            }).ToList();
+            });
 
             ViewData["VehicleTypes"] = vehicleTypes;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Statistics()
-        {
-            var parkedVehicles = await _context.Vehicle
-                .Where(v => _context.ParkingLot.Any(pt => pt.VehicleId == v.Id))
-                .Include(v => v.VehicleType)
-                .ToListAsync();
-
-            var vehicleTypeCount = new Dictionary<string, int>();
-
-            foreach (var vehicle in parkedVehicles)
-            {
-                if (vehicle.VehicleType != null)
-                {
-                    if (!vehicleTypeCount.ContainsKey(vehicle.VehicleType.Type))
-                    {
-                        vehicleTypeCount[vehicle.VehicleType.Type] = 0;
-                    }
-
-                    vehicleTypeCount[vehicle.VehicleType.Type]++;
-                };
-            }
-
-            var totalWheels = parkedVehicles.Sum(v => v.NrOfWheels);
-            var totalRevenue = _context.Receipt.Sum(r => r.Price);
-
-            ViewBag.vehicleTypeCount = vehicleTypeCount;
-            ViewBag.TotalWheels = totalWheels;
-            ViewBag.TotalRevenue = totalRevenue.ToString("#,##0.00");
-
-
-            return View();
         }
     }
 }
